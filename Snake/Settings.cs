6b979fea6fddac215578b11f4e0d7517d023ba66
Kryspin Kucha <blackcaer@onet.pl>
2023-07-10 17:14:44 +0200

    Settings documented and corrected

--- Snake/Settings.cs
+++ Snake/Settings.cs
@@ -8,6 +8,7 @@ namespace Snake
 {
     public class Settings
     {
+        // Constants
         public const int tickTimeStart = 100;
         public const int minSideCells = 2;
         public const int maxSideCells = 30;
@@ -15,8 +16,11 @@ namespace Snake
         public const double maxTickTimeMultiplier = 10;
         public const int defaulSideCells = 15;
 
+        // When SettingsFreezed flag is true, when the X flag is false, methods,
+        // that change the properties of a setting return immediately
         public bool SettingsFreezed { get; private set; } = false;
 
+        // Properties which can be changed while SettingsFreezed is false:
         public double TickTimeMultiplier { get; private set; } = 1.0;
         public int Rows { get; private set; }
         public int Cols { get; private set; }
@@ -27,9 +31,32 @@ namespace Snake
             Cols = defaulSideCells;
         }
 
-        public Settings(Settings obj) : this()
+        public void FreezeSettings()
         {
-            TickTimeMultiplier  = obj.TickTimeMultiplier;
+            SettingsFreezed = true;
+        }
+
+        public void UnfreezeSettings()
+        {
+            SettingsFreezed = false;
+
+        }
+
+        /// <summary>
+        /// Copies other Settings to this Settings (if !SettingsFreezed)
+        /// </summary>
+        /// <param name="settingsToBeApplied"> Settings object to be pasted</param>
+        /// <returns> bool indicating if action succeeded</returns>
+        public bool ApplySettings(Settings settingsToBeApplied)
+        {
+            if (SettingsFreezed)
+                return false;
+
+            TickTimeMultiplier = settingsToBeApplied.TickTimeMultiplier;
+            Rows = settingsToBeApplied.Rows;
+            Cols = settingsToBeApplied.Cols;
+
+            return true;
         }
 
         public void SetTickTimeMultiplier(double NewTickTimeMultiplier)
@@ -49,18 +76,9 @@ namespace Snake
         {
             if (SettingsFreezed)
                 return;
+
             Rows = (NewRows >= minSideCells && NewRows <= maxSideCells) ? NewRows : defaulSideCells;
             Cols = (NewCols>=minSideCells && NewCols <= maxSideCells) ? NewCols : defaulSideCells;
-        }
-
-        public void FreezeSettings()
-        {
-            SettingsFreezed = true;
-        }
-
-        public void UnfreezeSettings()
-        {
-            SettingsFreezed = false;
         }
 
     }
