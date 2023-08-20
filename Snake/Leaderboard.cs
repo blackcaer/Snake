6dd80c7d9fb6374a585f8f0ca60d3987dd5cb268
blackcaer <kryspin123@onet.pl>
2023-08-21 00:53:11 +0200

    transferred to ObservableCollection

--- Snake/Leaderboard.cs
+++ Snake/Leaderboard.cs
@@ -11,36 +11,29 @@ namespace Snake
 {
     public class Leaderboard
     {
-        public List<PlayerScore> PlayersScores { get; private set; } = new();
+        public ObservableCollection<PlayerScore> PlayersScores { get; private set; } = new();
         public string FileName { get; private set; }
         public int MaxPlayerCount { get; private set; }
 
         public Leaderboard(string fileName = "", int maxPlayerCount = 10)
         {
-
-            SetFileName(fileName);
-            LoadFromFile();
+            SetFileName(fileName); 
             SetMaxPlayerCount(maxPlayerCount);
         }
         public void AddPlayerScore(PlayerScore playerScore)
         {
-            int index = (PlayersScores).BinarySearch(
-                playerScore, PlayerScore.BestScoreComparer);
-
-            if (index < 0)
-                index = ~index;
-            //int index = 0; //TODO repair
-            PlayersScores.Insert(index, playerScore);
+            PlayersScores.Add(playerScore);
+            SortLeaderboard();
             TrimExcessScores();
         }
         private void TrimExcessScores()
         {
-            if (PlayersScores.Count > MaxPlayerCount)
-                PlayersScores.RemoveAt(PlayersScores.Count - 1);
+            while (PlayersScores.Count > MaxPlayerCount)
+                PlayersScores.RemoveAt(0);
         }
         public void SetMaxPlayerCount(int maxPlayerCount)
         {
-            MaxPlayerCount = maxPlayerCount;
+            MaxPlayerCount = (maxPlayerCount >= 1) ? maxPlayerCount : 1;
         }
         public void SetFileName(string fileName)
         {
@@ -66,14 +59,21 @@ namespace Snake
             try
             {
                 string fileContent = File.ReadAllText(FileName);
-                PlayersScores = JsonConvert.DeserializeObject<List<PlayerScore>>(fileContent);
-                //PlayersScores = JsonConvert.DeserializeObject<ObservableCollection<PlayerScore>>(fileContent);
+                PlayersScores = JsonConvert.DeserializeObject<ObservableCollection<PlayerScore>>(fileContent);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error while loading Leaderboard: {ex.Message}");
             }
-
+            TrimExcessScores();
+            SortLeaderboard();
+        }
+        public void SortLeaderboard()
+        {
+            var tmp = new ObservableCollection<PlayerScore>(
+            PlayersScores.OrderByDescending(score => score, PlayerScore.BestScoreComparer));
+            PlayersScores.Clear();
+            foreach (PlayerScore score in tmp) PlayersScores.Add(score);
         }
     }
 }
